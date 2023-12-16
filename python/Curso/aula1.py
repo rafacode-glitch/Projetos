9e2e280d2c9cb658b85f1fd1a3ce9fd3458307f8
Rafael Lourival <rafaellourivaldasilva@gmail.com>
2023-12-16 05:54:24 -0300

    primeiros códigos

---
+++ python/Curso/aula1.py
@@ -0,0 +1,7 @@
+# Função print permite exibir um texto na tela
+print('Hello, world!')
+
+'''
+Testando docstrings como comentário
+isso não é um comentário oficial de python e sim uma "gambiarra" e o interpretador python consegue ler.
+'''
