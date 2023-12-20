180aa7f7e98a3c2c62288b2a5454be46731d7246
Rafael Lourival <rafaellourivaldasilva@gmail.com>
2023-12-19 23:28:33 -0300

    exec2

---
+++ python/Curso/exec2.py
@@ -0,0 +1,28 @@
+# Primeira forma
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura, 'de altura, '  "pesa",
+      peso, 'quilos ' 'e seu IMC é ', imc)
+
+# Segunda forma
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura, '  "pesa",
+      peso, 'quilos ' 'e seu IMC é ', imc)
+
+
+# Forma do Otávio
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos ' 'e seu IMC é',)
+print(imc)
