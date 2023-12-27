9965c72b7891a00a89acdc3745f8ed427e2ca495
Rafael Lourival <rafaellourivaldasilva@gmail.com>
2023-12-27 11:06:02 -0300

    exec4 revisado

--- python/Curso/exec4.py
+++ python/Curso/exec4.py
@@ -1,7 +1,18 @@
-nome = input('Digite seu nome: ')
+# não consegui resolver sozinho por esquecer algumas coisas
+nome = input('Digite o seu nome: ')
 idade = input('Digite sua idade: ')
 
-# if nome and idade:
-#   print('Seu nome é {nome}')
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
 
-print('{nome}')
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome NÃO contém espaços')
+
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
+else:
+    print("Desculpe, você deixou campos vazios.")
