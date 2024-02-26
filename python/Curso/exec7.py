2d7b6c96e3177f31271a43f390f79ab81f36bfc5
Rafael Lourival <rafaellourivaldasilva@gmail.com>
2024-02-26 11:01:13 -0300

    .

--- python/Curso/exec7.py
+++ python/Curso/exec7.py
@@ -5,6 +5,8 @@ while True:
     operador = input('Digite o operador (+-/*): ')
 
     numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
 
     try:
         num_1_float = float(numero_1)
@@ -13,19 +15,32 @@ while True:
     except:
         numeros_validos = None
 
-        if numeros_validos is None:
-            print('Um ou ambos os números digitados são inválidos.')
-            continue
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
 
-        operadores_permitidos = '+-/*'
+    operadores_permitidos = '+-/*'
 
-        if operador not in operadores_permitidos:
-            print('Operador inválido')
-            continue
+    if operador not in operadores_permitidos:
+        print('Operador inválido')
+        continue
 
-        if len(operador) > 1:
-            print('Digite apenas um operador.')
-            continue
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    print('Realizando sua conta. confira o resultado abaixo')
+
+    if operador == '+':
+        print(f'{num_1_float}+{num_2_float}=', num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'{num_1_float}-{num_2_float}=', num_1_float - num_2_float)
+    elif operador == '/':
+        print(f'{num_1_float}/{num_2_float}=', num_1_float / num_2_float)
+    elif operador == '*':
+        print(f'{num_1_float}*{num_2_float}=', num_1_float * num_2_float)
+    else:
+        print('Nunca deveria chegar aqui.')
 
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
 
